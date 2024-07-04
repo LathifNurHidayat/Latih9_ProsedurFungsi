b52d87c1cd60530d85620d80ba74887463753009
LathifNurHidayat <151482653+LathifNurHidayat@users.noreply.github.com>
2024-07-04 11:55:30 +0700

    Add files via upload

---
+++ Latih9_ProsedurFungsi/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Latih9_ProsedurFungsi
+{
+     class Program
+    {
+        static void TampilkanWaktu()
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Sekarang  :"+date );
+
+        }
+       
+
+        static int Kali(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void Main()
+        {
+            int hasil = Kali(5, 4);
+            Console.WriteLine("Hasil Perkalian : " + hasil);
+        }
+
+        static bool ApakahGenap(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+    }
+
